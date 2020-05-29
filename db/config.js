c434628d3e0bb0b41e14f91bf23dc77bef047e68
Menglong.Li <116938310@qq.com>
2020-05-29 23:57:31 +0800

    mysql-promise

--- db/config.js
+++ db/config.js
@@ -1,16 +1,3 @@
-// const mysql = require('mysql')
-// const connection = mysql.createPool({
-//     host:'localhost',           //数据库地址
-//     user: 'root',               //用户名
-//     password: '116938310',           //密码
-//     port : '3306',              //端口
-//     database: 'vue',           //库名
-//     multipleStatements:true     //允许执行多条语句
-// })
-//
-// module.exports = connection;
-
-
 var Mysql = require('node-mysql-promise');
 var mysql = Mysql.createConnection({
     host:'localhost',           //数据库地址
