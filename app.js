74f5458363c5a80c49e30b64810d0b0903dd6142
Menglong.Li <116938310@qq.com>
2020-06-02 00:59:35 +0800

    修改

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Content-Type', 'application/json;charset=utf-8');
   if(req.method == 'OPTIONS') {
     res.sendStatus(200);
