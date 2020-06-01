3e159836228347d316712fca00f57fc056333564
Menglong.Li <116938310@qq.com>
2020-06-02 01:01:36 +0800

    完成新增，修改

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,62 @@
 var mysql = require('../db/config');
+var md5 = require('md5-node');
+var moment = require('moment');
 
 module.exports = {
+    /**
+     * 登录
+     * @param params 用户名及密码
+     * @returns {Promise|*}
+     */
     login: function (params) {
         let {username,pass} = params;
         return mysql.table('admin').where({username:username,password:pass}).find();
+    },
+    /**
+     * 新增数据，同时验证用户名是否存在
+     * @param params
+     * @returns {*|Bluebird.Promise|Promise}
+     */
+    thenAdd: (params) => {
+        //密码转md5
+        params.password = md5(params.password);
+        params.times = moment().format('YYYY-MM-DD HH:mm:ss');
+        return mysql.table('admin').thenAdd(params,{username:params.username},true);
+    },
+    /**
+     * 修改信息
+     * @param params
+     * @returns {Promise.<*>}
+     */
+    edit: async (params) => {
+        //由于await不能获得reject，所以需要try抓捕一下
+        try{
+            //先取出原数据
+            let old = await mysql.table('admin').where({id:params.id}).find();
+            if(JSON.stringify(old) != '{}') {
+                //需要更新密码
+                if(old.password != params.password) {
+                    params.password = md5(params.password);
+                }
+
+                let newdata = Object.assign(old,params);
+                //更新
+                return mysql.table('admin').where({id:old.id}).update(newdata);//update返回影响行数
+            }else {
+                throw new Error('信息不存在');
+            }
+
+        }catch (err) {
+            return err.message;
+        }
+    },
+    /**
+     * 获取信息
+     * @param id
+     * @returns {Promise|*}
+     */
+    getView: id => {
+        return mysql.table('admin').where({id:id}).find();
     },
     getlist: function(req,res,next) {
         let params = req.query;
--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,10 @@ var custom = require('../controllers/custom');
 var jwt = require('jsonwebtoken');
 /* GET users listing. */
 
+/**
+ * 后台登录
+ */
 router.post('/login',(req,res,next) => {
-    console.log(req.body)
     let {username,pass} = req.body;
     if(custom.isEmpty(username) || custom.isEmpty(pass)) res.send('用户名或密码错误',500);
     controller.login(req.body).then(data => {
@@ -23,14 +25,59 @@ router.post('/login',(req,res,next) => {
                 },200)
             });
         }else {
-            res.send('用户名或密码错误',500);
+            res.send('用户名或密码错误',403);
         }
     }).catch(err => {
-        res.send('用户名或密码错误',500);
+        res.send('用户名或密码错误',403);
     })
 })
 
-router.get('/setting/getlist', function(req, res, next) {
+/**
+ * 新增管理员
+ */
+router.post('/admin/register',(req,res,next) => {
+    let {username,password,name,qx} = req.body;
+    if(custom.isEmpty(username) || custom.isEmpty(password)) res.send('用户名或密码不能为空',400);
+    //验证用户名是否存在并插入
+    controller.thenAdd(req.body).then(result => {
+        if(result.type == 'exist') {
+            res.send('用户名已存在',403);
+        }else {
+            res.send('ok');
+        }
+    })
+});
+
+/**
+ * 编辑或查看
+ */
+router.get('/admin/getview/:id',(req,res,next) => {
+    controller.getView(req.params.id).then(result => {
+        if(result.id > 0) {
+            res.send(result);
+        }else {
+            res.send('信息不存在',403);
+        }
+    });
+});
+
+/**
+ * 更新
+ */
+router.put('/admin/edit', (req,res,next) => {
+    controller.edit(req.body).then(result => {
+        if(result > 0) {
+            res.send('ok');
+        }else {
+            res.send(result,403);
+        }
+    });
+});
+
+/**
+ * 获取列表
+ */
+router.get('/admin/getlist', function(req, res, next) {
     controller.getlist(req,res,next).then(data => {
         let results = {
             list: data,
