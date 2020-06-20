f8c355bd637ebd04fc76423de564c3caa2924644
Menglong.Li <116938310@qq.com>
2020-06-20 22:16:38 +0800

    小修

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var dbc = require('../db/db');
+var controller = require('../controllers/admin');
+var custom = require('../controllers/custom');
+var jwt = require('jsonwebtoken');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  const sql = 'select * from users';
-  dbc.query(sql).then(data => {
-    console.log(data);
-    res.render('index', { title: JSON.stringify(data) });
-  })
-
-});
+/**
+ * 后台登录
+ */
+router.post('/login',(req,res,next) => {
+    let {username,pass} = req.body;
+    if(custom.isEmpty(username) || custom.isEmpty(pass)) res.send('用户名或密码错误',500);
+    controller.login(req.body).then(data => {
+        if(data.id > 0){
+            let tokenInfo = {
+                iss: '鹅这个彪崽',//签发人
+                id:data.id,
+                username:data.username
+            }
+            let token = jwt.sign(tokenInfo,'Bearer ',{expiresIn:3600},(err,token) => {
+                if(err) throw err;
+                res.json({
+                    token:'Bearer ' + token
+                },200)
+            });
+        }else {
+            res.send('用户名或密码错误',403);
+        }
+    }).catch(err => {
+        res.send('用户名或密码错误',403);
+    })
+})
 
 module.exports = router;
