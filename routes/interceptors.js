735b267afa7fbe45187850d8e95dd14e9a70bee2
Menglong.Li <116938310@qq.com>
2020-06-20 22:16:21 +0800

    小修

--- routes/interceptors.js
+++ routes/interceptors.js
@@ -9,14 +9,18 @@ router.use((req,res,next) => {
     if(req.url.split('?')[0] == '/api/Login') {
         next(); //登录页面不需要验证token
     } else {
-        const token = req.headers['authorization'].split(' ').pop();
-        jwt.verify(token,'Bearer ',(err,data) => {
-            if(err) {
-                res.send('请求失败：未验证的Token',401);
-            }else {
-                next();
-            }
-        })
+        if(req.headers['authorization'] == undefined || req.headers['authorization'] == '') {
+            res.send('Error:token does not exist',401)
+        }else {
+            const token = req.headers['authorization'].split(' ').pop();
+            jwt.verify(token,'Bearer ',(err,data) => {
+                if(err) {
+                    res.send('请求失败：未验证的Token',401);
+                }else {
+                    next();
+                }
+            })
+        }
     }
 })
 
