7f60335af12a788ffc7665b3170fe2c904cb0417
Menglong.Li <116938310@qq.com>
2020-06-23 23:01:41 +0800

    增加行为日志

--- controllers/webset.js
+++ controllers/webset.js
@@ -1,17 +1,18 @@
 var mysql = require('../db/config');
+let logs = require('./adminlogs')
 
 module.exports = {
     getView: () => {
         return mysql.table('webset').where({id:1}).find();
     },
-    put: async (params) => {
+    put: async (req,res,next) => {
         let old = await mysql.table('webset').where({id:1}).find();
         if(JSON.stringify(old) != '{}') {
-            console.log('存在')
-            return mysql.table('webset').where({id:1}).update(params);
+            logs.inlogs('修改参数设置')
+            return mysql.table('webset').where({id:1}).update(req.body);
         }else {
-            console.log('不存在')
-            return mysql.table('webset').add(params);
+            logs.inlogs('新增参数设置')
+            return mysql.table('webset').add(req.body);
         }
     }
 }
