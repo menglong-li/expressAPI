d82d14c03291afcf18387825e7a4ea4de3862386
Menglong.Li <116938310@qq.com>
2020-06-28 00:24:49 +0800

    商品分类

--- controllers/productType.js
+++ controllers/productType.js
@@ -1,18 +1,45 @@
-let mysql = require('../db/config')
+let mysql = require('../db/config');
+let date = require('moment')
 
 module.exports = {
-    getlist: async (req,res,next)=>{
-        let {title,current,size} = req.query;
+    getlist: (req,res,next)=>{
+        let {title} = req.query;
         let where = '';
         if(title != undefined) {
             where = {title:['like','%' + title + '%']};
         }
-        let data = await mysql.table('producttype').where(where).page(current,size).select();
-        let total = await mysql.table('producttype').count();
-        let result = {
-            data: data,
-            total: total
-        }
-        res.send(result);
+        mysql.table('producttype').where(where).order('pid asc,sort asc').select().then(result=> {
+            res.send(result);
+        });
+    },
+    add: (req,res,next) => {
+        let params = req.body;
+        params.times = date().format('YYYY-MM-DD HH:mm:ss')
+        mysql.table('producttype').add(params).then(result => {
+            res.send('ok');
+        }).catch((err)=> {
+            res.send(err,403);
+        });
+    },
+    getview: (req,res,next) => {
+        let id = req.params.id;
+        mysql.table('producttype').where({id:id}).find().then(result=> {
+            res.send(result);
+        }).catch(error => {
+            res.send(error,403);
+        });
+    },
+    edit: (req,res,next) => {
+        let model = req.body;
+        mysql.table('producttype').where({id:model.id}).update(model).then(result=> {
+            if(result > 0) {
+                res.send('ok');
+            }else {
+                res.send('操作失败',403);
+            }
+        });
+    },
+    delete: (req,res) => {
+        //如果包含子级类别，提示请先删除子级类别
     }
 }
--- routes/producttype.js
+++ routes/producttype.js
@@ -4,4 +4,12 @@ let controller = require('../controllers/productType');
 
 router.get('/api/producttype/getlist',controller.getlist);
 
+router.post('/api/producttype/add',controller.add);
+
+router.get('/api/producttype/getview/:id',controller.getview);
+
+router.put('/api/producttype/edit',controller.edit);
+
+router.delete('/api/producttype/delete',controller.delete);
+
 module.exports = router;
