18336aa20e3fabc77b90cfc371f5147c33585e4a
Menglong.Li <116938310@qq.com>
2020-09-15 03:11:48 +0800

    列表信息优化

--- controllers/product.js
+++ controllers/product.js
@@ -4,11 +4,15 @@ let date = require('moment');
 module.exports = {
     getlist: async (req,res) => {
         let {title,current,size} = req.query;
-        let where = '';
+        let where = 'isSale=1';
         if(title != undefined) {
-            where = {title:['like','%' + title + '%']};
+            where += ' and like','%' + title + '%';
         }
-        let data = await mysql.table('product').where(where).page(current,size).order('sort desc,id desc').field('id,title,isSale,sort,times').select()
+        let data = await mysql.table('product').where(where).page(current,size).order('sort desc,id desc').field('id,title,photo,price,inventory,isSale,sort,times').select();
+        //因列表只显示头图，所以把photo数组取出来只返回[0];
+        data.map((item,index) => {
+            data[index].photo = JSON.parse(data[index].photo)[0];
+        });
         let total = await mysql.table('product').where(where).count();
         let result = {
             list: data,
