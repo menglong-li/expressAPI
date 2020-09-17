b8b6898528563c0b117d5237578aa762498704d1
Menglong.Li <116938310@qq.com>
2020-09-17 22:48:59 +0800

    添加接口

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,31 @@
 let mysql = require('../db/config');
 let date = require('moment');
+let logs = require('./adminlogs');
 
 module.exports = {
     getlist: async (req,res) => {
         let {title,current,size} = req.query;
         let where = 'isSale=1';
         if(title != undefined) {
-            where += ' and like','%' + title + '%';
+            where += ' and title like "%' + title + '%"';
+        }
+        let data = await mysql.table('product').where(where).page(current,size).order('sort desc,id desc').field('id,title,photo,price,inventory,isSale,sort,times').select();
+        //因列表只显示头图，所以把photo数组取出来只返回[0];
+        data.map((item,index) => {
+            data[index].photo = JSON.parse(data[index].photo)[0];
+        });
+        let total = await mysql.table('product').where(where).count();
+        let result = {
+            list: data,
+            total: total
+        }
+        res.send(result);
+    },
+    getdownlist: async (req,res) => {
+        let {title,current,size} = req.query;
+        let where = 'isSale=0';
+        if(title != undefined) {
+            where += ' and title like "%' + title + '%"';
         }
         let data = await mysql.table('product').where(where).page(current,size).order('sort desc,id desc').field('id,title,photo,price,inventory,isSale,sort,times').select();
         //因列表只显示头图，所以把photo数组取出来只返回[0];
@@ -24,9 +43,68 @@ module.exports = {
         let params = req.body;
         params.times = date().format('YYYY-MM-DD HH:mm:ss');
         mysql.table('product').add(params).then(result => {
+            logs.inlogs('发布商品：' + params.title);
             res.send('ok');
         }).catch((err)=> {
             res.send(err,403);
         });
-    }
+    },
+    down:(req,res,next) => {
+        let {id} = req.body;
+        mysql.table('product').where({id:['in',id]}).update({isSale:0}).then(result=> {
+            if(result > 0) {
+                logs.inlogs('下架商品ID=' + id);
+                res.send('ok');
+            }else {
+                res.send('操作失败',403);
+            }
+        })
+    },
+    up:(req,res,next) => {
+        let {id} = req.body;
+        mysql.table('product').where({id:['in',id]}).update({isSale:1}).then(result=> {
+            if(result > 0) {
+                logs.inlogs('重新上架商品ID=' + id);
+                res.send('ok');
+            }else {
+                res.send('操作失败',403);
+            }
+        })
+    },
+    delete:(req,res,next) => {
+        let {id,title} = req.query;
+        mysql.table('product').where({id:['in',id]}).delete().then(result=> {
+            if(result > 0) {
+                if(title.split(',').length > 1) {
+                    logs.inlogs('批量删除商品：' + title);
+                }else {
+                    logs.inlogs('删除商品：' + title);
+                }
+                res.send('ok');
+            }else {
+                res.send('操作失败',403);
+            }
+        })
+    },
+    getview:(req,res,next) => {
+        let {id} = req.params;
+        mysql.table('product').where({id:id}).find().then((result) => {
+            if(JSON.stringify(result) != '{}') {
+                res.send(result);
+            }else {
+                res.send('操作失败',403);
+            }
+        });
+    },
+    edit: (req,res,next) => {
+        let model = req.body;
+        mysql.table('product').where({id:model.id}).update(model).then(result=> {
+            if(result > 0) {
+                logs.inlogs('编辑商品：' + model.title);
+                res.send('ok');
+            }else {
+                res.send('操作失败',403);
+            }
+        });
+    },
 }
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,12 @@ let router = express.Router();
 let controller = require('../controllers/product');
 
 router.get('/api/product/getlist',controller.getlist);
+router.get('/api/product/getdownlist',controller.getdownlist);
+router.get('/api/product/getview/:id',controller.getview);
 router.post('/api/product/add',controller.add);
+router.put('/api/product/down',controller.down);
+router.put('/api/product/up',controller.up);
+router.put('/api/product/edit',controller.edit);
+router.delete('/api/product/delete',controller.delete);
 
 module.exports = router;
